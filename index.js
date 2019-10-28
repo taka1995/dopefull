fdd6805905999fdf2e45c8a53781c4acfbd122dc
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-10-28 15:12:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ showTimes = () => {
   return result;
 }
 
+
+"dependencies": {
+    "cool-ascii-faces": "^1.3.4",
+    "ejs": "^2.5.6",
+    "express": "^4.15.2",
+    "pg": "^7.12.1"
+  },
+    
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
