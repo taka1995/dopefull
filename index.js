c950ce954b4155f43551df4b0fd881fab063a9ee
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-10-29 00:04:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ app.listen(app.get('port'), function() {
 
 module.exports = app
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 
