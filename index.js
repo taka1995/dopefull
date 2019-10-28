77cb85ca87afe178552ace6d6c0eb6660ce68877
taka1995 <38433955+taka1995@users.noreply.github.com>
2019-10-28 14:27:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ app.listen(app.get('port'), function() {
 
 module.exports = app
 
+.get('/times', (req, res) => res.send(showTimes()))
+
+showTimes = () => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result;
+}
 
 
 
